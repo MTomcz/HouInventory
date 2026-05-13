36bfd5d369cb7d36630d94385124aa5ae80a4f01
Marie Tomczyk Larsen <mtla79134@edu.ucl.dk>
2026-05-13 15:27:09 +0200

    Constructer

--- HuInventory/Inventory.cs
+++ HuInventory/Inventory.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace HuInventory
 {
     internal class Inventory
     {
+
+
     }
+
 }
--- HuInventory/InventoryItem.cs
+++ HuInventory/InventoryItem.cs
@@ -6,5 +6,29 @@ namespace HuInventory
 {
     internal class InventoryItem
     {
-    }
+		public enum Category
+		{
+			Dryfoods,
+			Frozenfoods,
+
+
+		}
+		public string Name { get; set; }
+		public Category ItemCategory { get; set; }
+		public int Amount { get; set; }
+		public int CurrentInv { get; set; }
+		public int MinInv { get; set; }
+		public DateTime ExpDate { get; set; }
+
+		public InventoryItem(string name, Category category, int amount, int currentInv, int minInv, DateTime expDate)
+		{
+			Name = name;
+			ItemCategory = category;
+			Amount = amount;
+			CurrentInv = currentInv;
+			MinInv = minInv;
+			ExpDate = expDate;
+
+		}
+	}
 }
