20e86161b114ced4362d0529573211f059c9ad5f
Michal Lachut <mila80831@edu.ucl.dk>
2026-05-18 20:09:04 +0200

    changed StockTransaction to SystemLog

---
+++ HuInventory/SystemLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HuInventory
+{
+    internal class StockTransaction
+    {
+        public string Username { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string TransactionType { get; set; } // "Login", "Logout", "AddStock", "RemoveStock"
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+
+        public StockTransaction(string username, string transactionType, string itemName = "", int quantity = 0)
+        {
+            Username = username;
+            Timestamp = DateTime.Now;
+            TransactionType = transactionType;
+            ItemName = itemName;
+            Quantity = quantity;
+        }
+    }
+}
